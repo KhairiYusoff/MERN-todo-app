961af2b4687e5588328c79b5ce387aa4c87bd26a
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-10-11 21:43:21 +0800

    update

--- api/server.js
+++ api/server.js
@@ -6,3 +6,19 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Connected to DB"))
+    .catch(console.error);
+
+const Todo = require('./models/Todo')
+
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+})
+
+app.listen(3000, () => console.log("Server started on port 3000"))
